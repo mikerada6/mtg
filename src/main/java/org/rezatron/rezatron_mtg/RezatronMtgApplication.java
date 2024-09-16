e15b9ffbb9e5c4031b69cd37f46fbdd30162af22
Radaszkiewicz, Michael <Michael_Radaszkiewicz@comcast.com>
2024-09-16 19:22:14 -0400

    initial commit

---
+++ src/main/java/org/rezatron/rezatron_mtg/RezatronMtgApplication.java
@@ -0,0 +1,13 @@
+package org.rezatron.rezatron_mtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezatronMtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezatronMtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rezatron/rezatron_mtg/RezatronMtgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rezatron.rezatron_mtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezatronMtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
